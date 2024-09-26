de73375d5eeca5e1d5a73d1538c02ed45c4f57d1
JAI PATHAK <64726553+jaipathak1972@users.noreply.github.com>
2024-09-26 17:07:00 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='speechcare',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
